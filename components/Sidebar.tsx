09c55c6b82acff6daf08eeaadeba1e1d872ddef0
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-01-10 18:45:21 +1300

    ✨ Add components to Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,9 +5,15 @@ import Link from 'next/link';
 import GoogleLogin from 'react-google-login';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
+import Discover from './Discover';
+import SuggestedAccount from './SuggestedAccount';
+import Footer from './Footer';
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
+
+  const userProfile = false;
+
   const normalLink =
     'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded';
 
@@ -31,6 +37,33 @@ const Sidebar = () => {
               </div>
             </Link>
           </div>
+          {!userProfile && (
+            <div className='px-2 py-4 hidden xl:block'>
+              <p className=' text-gray-500'>
+                Log in to like adn comment on videos
+              </p>
+              <div className='pr-4'>
+                <GoogleLogin
+                  clientId=''
+                  render={(renderProps) => (
+                    <button
+                      className='cursor-pointer bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]'
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      Log in
+                    </button>
+                  )}
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy='single_site_origin'
+                />
+              </div>
+            </div>
+          )}
+          <Discover />
+          <SuggestedAccount />
+          <Footer />
         </div>
       )}
     </div>
