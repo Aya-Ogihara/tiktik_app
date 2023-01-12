d32b5bef8a1375b9064a4f965ed80366bd8b625f
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-01-12 21:42:45 +1300

    ✨ Connect api

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,25 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '../styles/Home.module.css';
+import type { NextPage } from 'next';
+import axios from 'axios';
 
-const inter = Inter({ subsets: ['latin'] });
+//const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </div>
   );
-}
+};
+
+export const getServerSideProps = async () => {
+  const response = await axios.get('http://localhost:3000/api/post');
+  console.log(response.data.name);
+  return {
+    props: {},
+  };
+};
+
+export default Home;
