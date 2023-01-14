973d99e51606f05520f7cbd544b7be6537385919
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-01-14 21:30:47 +1300

    ✨ Add NoResults and VideoCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,36 @@ import { Inter } from '@next/font/google';
 import styles from '../styles/Home.module.css';
 import type { NextPage } from 'next';
 import axios from 'axios';
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
+import VideoCard from '../components/VideoCard';
 
 //const inter = Inter({ subsets: ['latin'] });
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  //console.log(videos);
+
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={'No Videos... :('} />
+      )}
     </div>
   );
 };
 
 export const getServerSideProps = async () => {
-  const response = await axios.get('http://localhost:3000/api/post');
-  console.log(response.data.name);
+  const { data } = await axios.get('http://localhost:3000/api/post');
   return {
-    props: {},
+    props: {
+      videos: data,
+    },
   };
 };
 
