da2fb1b0a67f3292e8ab26f60f1bfbd595e90cbb
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-01-15 11:25:46 +1300

    🚧 Implement VideoCard component

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,13 +1,70 @@
-import React from 'react';
-import { Video } from '../types';
+import React, { useState, useEffect, useRef } from 'react';
 import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+
+import { Video } from '../types';
 
 interface IProps {
   post: Video;
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
-  return <div>VideoCard</div>;
+  return (
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+      {/* Post info */}
+      <div>
+        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+          <div className='w-10 h-10 md:w-16 md:h-16'>
+            <Link href='#'>
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  alt='User icon'
+                  className='rounded-full'
+                  src={post.postedBy.image}
+                  layout='responsive'
+                />
+              </>
+            </Link>
+          </div>
+          <div>
+            <Link href='#'>
+              <div className='flex items-center gap-2'>
+                <p className='flex items-center gap-2 md:text-md font-bold text-primary'>
+                  {post.postedBy.userName}{' '}
+                  <GoVerified className='text-blue-400 text-md' />
+                </p>
+                <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                  {post.postedBy.userName}
+                </p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* Post video */}
+      <div className='lg:ml-20 flex gap-4 relative'>
+        <div
+          className='rounded-3xl'
+          onMouseEnter={() => {}}
+          onMouseLeave={() => {}}
+        >
+          <Link href='#'>
+            <video
+              loop
+              src={post.video.asset.url}
+              className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
+            ></video>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoCard;
