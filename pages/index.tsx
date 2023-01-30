2e6735794d4eb2c3d6827993a28a015e3777aea3
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-01-30 21:51:46 +1300

    ⚡️ Replace URL variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 import { Video } from '../types';
 import NoResults from '../components/NoResults';
 import VideoCard from '../components/VideoCard';
+import { BASE_URL } from '../utils';
 
 //const inter = Inter({ subsets: ['latin'] });
 
@@ -29,7 +30,7 @@ const Home = ({ videos }: IProps) => {
 };
 
 export const getServerSideProps = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/post');
+  const { data } = await axios.get(`${BASE_URL}/api/post`);
   return {
     props: {
       videos: data,
