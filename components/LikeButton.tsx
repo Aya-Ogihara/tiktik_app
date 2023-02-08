21bae3d68542c7da03f12a43c7a6c6eccae884a1
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-02-08 22:07:47 +1300

    ✨ Create new components

---
+++ components/LikeButton.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { MdFavorite } from 'react-icons/md';
+import useAuthStore from '../store/authStore';
+
+interface IProps {
+  handleLike: () => void;
+  handleDisLike: () => void;
+}
+
+const LikeButton = ({ handleLike, handleDisLike }: IProps) => {
+  const [alreadyLiked, setAlreadyLiked] = useState(true);
+  const { userProfile } = useAuthStore();
+
+  return (
+    <div className='gap-6'>
+      <div className='mt-4 flex flex-col justify-center items-center cursor-pointer'>
+        {alreadyLiked ? (
+          <div
+            className=' bg-primary rounded-full p-2 md:p-4 text-[#F51997]'
+            onClick={handleDisLike}
+          >
+            <MdFavorite className='text-lg md:text-2xl' />
+          </div>
+        ) : (
+          <div
+            className=' bg-primary rounded-full p-2 md:p-4 text-gray-500'
+            onClick={handleLike}
+          >
+            <MdFavorite className='text-lg md:text-2xl' />
+          </div>
+        )}
+      </div>
+      <p className='text-md font-semi'>likes?.length || 0</p>
+    </div>
+  );
+};
+
+export default LikeButton;
