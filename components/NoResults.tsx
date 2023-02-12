eb944f300b4f60531eca614843e18f76eb249620
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-02-12 17:17:58 +1300

    💄 Add more suitable icon for no comment

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import React from 'react';
 import { MdOutlineVideocamOff } from 'react-icons/md';
+import { BiCommentX } from 'react-icons/bi';
 interface IProps {
   text: string;
 }
@@ -9,7 +10,11 @@ const NoResults: NextPage<IProps> = ({ text }) => {
   return (
     <div className='flex flex-col justify-center items-center h-full w-full'>
       <p className='text-8xl'>
-        <MdOutlineVideocamOff />
+        {text === 'Be the first one to add a comment.' ? (
+          <BiCommentX />
+        ) : (
+          <MdOutlineVideocamOff />
+        )}
       </p>
       <p className='text-2xl'>{text}</p>
     </div>
